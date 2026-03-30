71ef0cd73c0c95482cf4f8a6d7b1a77914043710
Uday9316 <udaydhorajiya@gmail.com>
2026-03-30 19:22:25 +0530

    "update"

--- src/constants/index.js
+++ src/constants/index.js
@@ -102,7 +102,7 @@ const navLinks = [
   const techStack = [
     {
       category: "Frontend",
-      items: ["React.js", "TypeScript", "Tailwind", "GSAP", "Shadcn/ui"],
+      items: ["React.js", "TypeScript", "Tailwind", "GSAP"],
     },
     {
       category: "Mobile",
